1275fab95dbbef8c490a50bca798c00984b7998d
reed_sin <grid.haan@gmail.com>
2022-01-15 10:43:14 +0900

    커밋테스트0115

---
+++ components/Mylayout.js
@@ -0,0 +1,11 @@
+import Header from './Header';
+
+
+const Layout = props => (
+  <>
+      <Header />
+      {props.children}
+      </>
+  )
+  
+  export default Layout
--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Layout from '../components/Mylayout'
 
-export default function Home() {
+export default function about() {
   return (
     <>
-     <Layout />
-  </>
-)
-}
+        hello im about!!
+    </>
+  )}
